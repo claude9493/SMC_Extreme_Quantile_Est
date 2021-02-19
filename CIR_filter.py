85b6acfe4a4c3f3591cf7c1a3ccc1e6c1c1edd39
claudezhang <claude9493@users.noreply.github.com>
2021-02-19 20:33:19 +0800

    Big update CIR_filter.py
    1. Move the repeated_simulation part in to one independent file cause it cannot be done in Jupyter environment.
    2. Start to use the evaluate module.

--- CIR_filter.py
+++ CIR_filter.py
@@ -18,6 +18,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from particles import state_space_models as ssm
 from sklearn.metrics import mean_squared_error
 
+import configparser
 import evaluate
 import filtering
 from model.CIR import CIR, CIR_mod, CIR_plot
@@ -32,61 +33,39 @@ MAX_INT_32 = np.iinfo(np.int32).max
 seeds = [np.random.randint(MAX_INT_32) for i in range(R)]
 
 #%% Define model and generate real data
+# Run this cell to generate data or run the second cell below to load data generated and saved before.
 cir = CIR()
 real_x, real_y = cir.simulate(T)
 fk_PF = ssm.GuidedPF(ssm=CIR(), data=real_y)
+CIR_plot(real_x)
 
 #%% Save the generated data
+# Run this cell to save the newly generated data into the Records/CIRYYYYmmdd-HHMMSS folder.
+
 name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-os.mkdir(f"./Records/CIR{name}")
-np.savez(file=f"./Records/CIR{name}/data", real_x=real_x, real_y=real_y)
-print(f"Data saved, CIR{name}")
+os.mkdir(f"./Records/CIR{name}")  # Create folder
+
+# Save the real_states plot
+plt.savefig(f"./Records/CIR{name}/real_states.png")
+# Save the generated data
+np.savez(file=f"./Records/CIR{name}/data", real_x=real_x, real_y=real_y)  
+# Save parameters of the model where data from.
+config = configparser.ConfigParser()
+config["PARAMETERS"] = {
+    **cir.default_params,
+    "Delta" : cir.Delta,
+    "H"     : cir.H
+}  
+with open(f"./Records/CIR{name}/config.ini", "w") as f:
+    config.write(f)
+
+print(f"Data saved in folder CIR{name}.")
 
 #%% Load data from saved npz file
-name = "20210204-225727"
-temp = np.load(file=f"./Records/CIR{name}/data.npz")
-real_x, real_y = temp.get("real_x"), temp.get("real_y")
+name = "20210204-225727"  # Modify the name manually
+loader = np.load(file=f"./Records/CIR{name}/data.npz")
+real_x, real_y = loader.get("real_x"), loader.get("real_y")
 fk_PF = ssm.GuidedPF(ssm=CIR(), data=real_y)
-
-#%% Plot the real_x
-CIR_plot(real_x)
-plt.savefig(f"./Records/CIR{name}/real_states.png")
-
-#%% Function for repeated filtering simulation
-default_args = {
-    "N": 100,
-    "ESSrmin": 1,
-    "resampling": "multinomial",
-    "store_history": True,
-    "compute_moments": False,
-    "online_smoothing": None,
-    "verbose": False,
-}
-
-
-def simulate_single(fk, alg_args=default_args):
-    t0 = time.time()
-    alg = particles.SMC(fk=fk, **alg_args)
-    alg.run()
-    t1 = time.time()
-    state_est = list(
-        map(lambda i: np.average(alg.hist.X[i], weights=alg.hist.wgts[i].W), range(T))
-    )
-    queue.put((state_est, t1 - t0))
-    # return state_est, t1-t0
-
-
-def repeated_simulation(fk, R=R, alg_args=default_args):
-    num_cores = multiprocessing.cpu_count()
-    f = partial(simulate_single, fk, alg_args)
-    res = Parallel(n_jobs=num_cores)(delayed(f)(i) for i in range(R))
-
-    state_ests = res[:, 0]
-    running_time = res[:, 1]
-
-    average_state_est = np.mean(state_ests, axis=0)
-    return average_state_est, np.mean(running_time)
-
 
 #%% Particle filter SMC_10K
 alg_PF = particles.SMC(
@@ -99,31 +78,30 @@ alg_PF = particles.SMC(
     online_smoothing=None,
     verbose=False,
 )
-
+# Run the SMC_10K for one time.
 filtering.state_est(alg_PF, real_x, name="(CIR, SMC_10K)", xmin=-2, xmax=10)
 plt.savefig(f"./Records/CIR{name}/SMC_10K_filtering_once.png")
 plt.show()
-
+# Display the "residual" plot of the filtering result.
 evaluate.resi_plot(alg_PF, real_x, "SMC_10K")
 plt.savefig(f"./Records/CIR{name}/SMC_10K_residuals_once.png")
 
-#%% SMC_10K repeated 10
-# average_state_est, average_running_time = repeated_simulation(fk_PF, R=10, alg_args=dict(default_args, N=10000))
-
 #%% SMC_10K 50 repeats
-# Ran in a single python file to avoid the inconsissence of Jupyter and multiprocessing
-evaluate.running_result_evaluate(
-    f"./Records/CIR{name}/running_result_10K_R50.npz", real_x, "SMC_10K"
+"""
+Repeated simulation is conducted in `CIR_filter_multiprocessing.py` in a parallelling way, and the corresponding results are saved in a npz file.
+"""
+# Load and analyze the simulation result.
+name = "20210204-225727"
+evaluate.result_evaluate(
+    f"./Records/CIR{name}/result_SMC_10K_R50.npz", real_x, "SMC_10K"
 )
+evaluate.tail_prob(f"./Records/CIR{name}/result_SMC_10K_R50.npz")
 
-
-#%% Particle filter SMC_100 (Truncated Normal proposal)
+#%% Particle filter SMC_100
 ## %%timeit
-# SMC_100, 100 particles each step
-# 13.2 s ± 1.2 s per loop (mean ± std. dev. of 7 runs, 1 loop each) for N = 1000
 alg_PF = particles.SMC(
     fk=fk_PF,
-    N=N,
+    N=N,  # 100
     ESSrmin=1,
     resampling="multinomial",
     store_history=True,
@@ -131,28 +109,42 @@ alg_PF = particles.SMC(
     online_smoothing=None,
     verbose=False,
 )
+
+# Run the SMC_100 for one time.
 filtering.state_est(alg_PF, real_x, name="(CIR, SMC_100)", xmin=-2, xmax=10)
 plt.savefig(f"./Records/CIR{name}/SMC_100_filtering_once.png")
-
+# Display the "residual" plot of the filtering result.
 evaluate.resi_plot(alg_PF, real_x, "SMC_100")
 plt.savefig(f"./Records/CIR{name}/SMC_100_residuals_once.png")
 
 #%% SMC_100 repeated
-# average_state_est, average_running_time = repeated_simulation(fk_PF, R=50)
-evaluate.running_result_evaluate(
-    f"./Records/CIR{name}/running_result_SMC100_R100.npz", real_x, "SMC_100"
+evaluate.result_evaluate(
+    f"./Records/CIR{name}/result_SMC100_R500.npz", real_x, "SMC_100"
 )
-plt.savefig(f"./Records/CIR{name}/SMC_100_average_est_{50}.png")
-
-# %% Tail probability
-all_particles = np.asarray(alg_PF.hist.X).reshape((100 * 100, 1))
-sns.distplot(all_particles)
-
+plt.savefig(f"./Records/CIR{name}/SMC_100_average_est_{500}.png")
 
 #%% Modified SMC_100 R=100
-evaluate.running_result_evaluate(
-    f"./Records/CIR{name}/running_result_ModifiedSMC100_R100.npz",
+evaluate.result_evaluate(
+    f"./Records/CIR{name}/result_ModifiedSMC100_R500.npz",
     real_x,
     "ModifiedSMC_100",
 )
+
+#%% SMC_100 with t proposal density R=10
+evaluate.result_evaluate(
+    f"./Records/CIR{name}/result_SMCt100_R500.npz",
+    real_x,
+    "SMCt_100",
+)
+# evaluate.tail_prob(f"./Records/CIR{name}/result_SMCt100_R500.npz")
+# %% tail probability 
+file_list = ["SMC100_R500", 
+             "ModifiedSMC100_R500", 
+             "SMCt100_R500", 
+             "SMC10K_R50"]
+file_list = [f"./Records/CIR{name}/result_"+f+".npz" for f in file_list]
+evaluate.tail_prob_multi(file_list, real_x)
+# %%
+# import importlib
+# importlib.reload(evaluate)
 
